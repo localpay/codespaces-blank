6948e55ea2544b1f60eabbd254df15ac2f3cef18
laylow861 <laylow861@gmail.com>
2023-11-09 12:03:23 +0900

    Delete source/js/service-key.js

--- source/js/service-key.js
+++
@@ -1,9 +0,0 @@
-// 공공데이터 포털 - API Service Key(공공데이터포털 마이페이지에서 Decoding 된 인증키)
-var apiServiceKey = "JSKs/8sVKqAW8vghyunm276WX7b4Kcy+CXEM6VwumKKDGzIDqWeW77XRU8rMtTuV8r0fBpPiI1OsdE29ONrkOg==";
-
-// Kakao 지도 API - App Key (javascript 키)
-var kakaoMapKey = "2446b760cf7e92ddee979f3b086e2f9b"
-
-// 사용처지역코드:
-// 특정 광역시/도 또는 시/군/구를 제한하여 검색서비스를 제공하고 싶을 경우, 해당 변수에 법정동 코드의 앞 5자리 입력 (지역별 코드는 `사용처지역코드.xlsx` 참고)
-var usageRgnCd = "41130";
